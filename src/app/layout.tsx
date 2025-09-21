e388201ebc62433e88173ec1da19fc605fae7836
root <debarghasaha16@gmail.com>
2025-09-21 08:39:03 +0000

    feat
    rss added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="alternate" type="application/rss+xml" href="/rss" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-dvh`}
       >
