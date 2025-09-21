29f3281979d48a5013480c895ab40c61b99f39bf
root <debarghasaha16@gmail.com>
2025-09-21 11:18:15 +0000

    feat
    durga banner added

--- src/actions/news.ts
+++ src/actions/news.ts
@@ -9,51 +9,44 @@ import {
   Data,
 } from "@/types/response";
 
+const origin = "http://210.79.128.182:8000";
+
 export async function getTopNews() {
-  const res = await fetch(
-    "https://master-news-service.onrender.com/api/index_delivery?intent=latest",
-    {
-      headers: {
-        "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
-      },
-      next: {
-        revalidate: 60 * 10,
-      },
-    }
-  );
+  const res = await fetch(`${origin}/api/index_delivery?intent=latest`, {
+    headers: {
+      "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
+    },
+    next: {
+      revalidate: 60 * 10,
+    },
+  });
 
   return (await res.json()) as ApiResponseWithoutPagination;
 }
 
 export async function getLatestNews() {
-  const res = await fetch(
-    "https://master-news-service.onrender.com/api/index_delivery?intent=recent",
-    {
-      headers: {
-        "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
-      },
-      next: { revalidate: 60 * 10 },
-    }
-  );
+  const res = await fetch(`${origin}/api/index_delivery?intent=recent`, {
+    headers: {
+      "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
+    },
+    next: { revalidate: 60 * 10 },
+  });
   return (await res.json()) as ApiResponseWithPagination;
 }
 
 export async function getTrendingNews() {
-  const res = await fetch(
-    "https://master-news-service.onrender.com/api/index_delivery?intent=most_read",
-    {
-      headers: {
-        "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
-      },
-      next: { revalidate: 60 * 10 },
-    }
-  );
+  const res = await fetch(`${origin}/api/index_delivery?intent=most_read`, {
+    headers: {
+      "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
+    },
+    next: { revalidate: 60 * 10 },
+  });
   return (await res.json()) as ApiResponseWithPagination;
 }
 
 export async function getVideoNews() {
   const res = await fetch(
-    "https://master-news-service.onrender.com/api/index_delivery?intent=recent_articles_with_videos",
+    `${origin}/api/index_delivery?intent=recent_articles_with_videos`,
     {
       headers: {
         "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
@@ -65,21 +58,18 @@ export async function getVideoNews() {
 }
 
 export async function getAdVideos() {
-  const res = await fetch(
-    "https://master-news-service.onrender.com/api/index_delivery?intent=ad_videos",
-    {
-      headers: {
-        "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
-      },
-      next: { revalidate: 60 * 10 },
-    }
-  );
+  const res = await fetch(`${origin}/api/index_delivery?intent=ad_videos`, {
+    headers: {
+      "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
+    },
+    next: { revalidate: 60 * 10 },
+  });
   return (await res.json()) as ApiResponseAdVideoWithPagination;
 }
 
 export async function getAdBannerImages() {
   const res = await fetch(
-    "https://master-news-service.onrender.com/api/index_delivery?intent=wide_ad_images",
+    `${origin}/api/index_delivery?intent=wide_ad_images`,
     {
       headers: {
         "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
@@ -91,22 +81,19 @@ export async function getAdBannerImages() {
 }
 
 export async function getNewsInfo(id: string) {
-  const res = await fetch(
-    `https://master-news-service.onrender.com/api/article/${id}`,
-    {
-      headers: {
-        "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
-      },
-      next: { revalidate: 60 * 10 },
-    }
-  );
+  const res = await fetch(`${origin}/api/article/${id}`, {
+    headers: {
+      "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
+    },
+    next: { revalidate: 60 * 10 },
+  });
 
   return (await res.json()) as Data;
 }
 
 export async function getCategoryWiseNews() {
   const res = await fetch(
-    "https://master-news-service.onrender.com/api/index_delivery?intent=category_wise_news",
+    `${origin}/api/index_delivery?intent=category_wise_news`,
     {
       headers: {
         "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
@@ -119,15 +106,12 @@ export async function getCategoryWiseNews() {
 }
 
 export async function getCategoryNewsInfo(id: string) {
-  const res = await fetch(
-    `https://master-news-service.onrender.com/api/category/${id}`,
-    {
-      headers: {
-        "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
-      },
-      next: { revalidate: 60 * 10 },
-    }
-  );
+  const res = await fetch(`${origin}/api/category/${id}`, {
+    headers: {
+      "Host-Id": "7a0e2ceb7b344f58a3245325440db44d",
+    },
+    next: { revalidate: 60 * 10 },
+  });
 
   return (await res.json()) as ApiResponseWithPagination;
 }
